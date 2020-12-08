adffa32c9b0015b0dc0248c7875eb115603a5f07
Luis Juan Ignacio Villalva <luisjivillalba@gmail.com>
2020-12-08 20:58:15 -0300

    base de datos completa(casi)

--- baseDeDatos/GYM.sql
+++ baseDeDatos/GYM.sql
@@ -1,55 +1,55 @@
-create database gimnasio;
+create database IF NOT EXISTS gimnasio;
 use gimnasio;
-create table rol (
+create table IF NOT EXISTS rol (
     idRol int(25) not null,
     descripcion varchar(25) not null,
     constraint pk_rol primary key (idRol)
-)ENGINE-InnoDb;
+)ENGINE=InnoDb;
 
 
-create table persona(
+create table IF NOT EXISTS persona(
     idPersona int(25) auto_increment not null,
     idRol int(25) not null,
     dni int(30) not null,
     nombre varchar(50) not null,
     apellido varchar(50) not null,
-    sexo varchar(1) check(sexo = 'F' or sexo = 'M'),
+    sexo varchar(1),
     tel varchar(20) null,
-    email varchar(60) null
+    email varchar(60) null,
     constraint pk_persona primary key (idPersona),
     constraint uq_tel unique (tel),
     constraint uq_email unique (email),
     constraint uq_dni unique (dni),
     constraint fk_rol foreign key (idRol) references rol (idRol)
-)ENGINE-InnoDb;
+)ENGINE=InnoDb;
 
-create table actividad(
+create table IF NOT EXISTS actividad(
     idActividad int not null,
     descripcion varchar (20) not null,
     constraint uq_descripcion unique(descripcion),
-    cosntraint pk_activiad primary key (idActividad)
-)ENGINE-InnoDb;
+    constraint pk_activiad primary key (idActividad)
+)ENGINE=InnoDb;
 
-create table ventas_cabecera(
+create table IF NOT EXISTS ventas_cabecera(
     idVCabecera int auto_increment not null,
     idPersona int not null,
     fecha date,
     total float,
     constraint pk_ventas_cabecera primary key (idVCabecera),
     constraint fk_ventas_persona foreign key (idPersona) references persona(idPersona)
-)ENGINE-InnoDb;
+)ENGINE=InnoDb;
 
-create table ventas_detalle(
+create table IF NOT EXISTS ventas_detalle(
     idVDetalle int auto_increment not null,
     idVCabecera int not null,
     idActividad int not null,
-    fecha-expiracion date,
+    fecha_expiracion date,
     constraint pk_ventas_detalle primary key (idVDetalle),
     constraint fk_ventas_detalle_cabecera foreign key (idVCabecera) references ventas_cabecera(idVCabecera),
     constraint fk_ventas_detalle_actividad foreign key (idActividad) references actividad(idActividad)
-)ENGINE-InnoDb;
+)ENGINE=InnoDb;
 
-create table estado(
+create table IF NOT EXISTS estado(
     idPersona int(25) not null,
     idActividad int not null,
     idEstado int not null,
@@ -57,4 +57,4 @@ create table estado(
     constraint fk_persona foreign key (idPersona) references persona(idPersona),
     constraint fk_actividad foreign key (idActividad) references actividad(idActividad),
     constraint pk_estado primary key (idPersona,idActividad,idEstado)
-)ENGINE-InnoDb;
+)ENGINE=InnoDb;
